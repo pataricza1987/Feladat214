a47a4127d11c35afbebf3e3a199e1341821b90ef
pataricza1987 <pataricza1987@gmail.com>
2026-06-17 20:16:15 +0200

    first commit

---
+++ Feladat214/KorongÁllapot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feladat214
+{
+    internal class KorongÁllapot
+    {
+    }
+}
